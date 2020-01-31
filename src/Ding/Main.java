ef46e114109ee1bfbea60d8368fb865363cfb82c
Robert McGuffin <rob.mcguffs@gmail.com>
2020-01-31 16:50:47 -0700

    Hello!

--- src/Ding/Main.java
+++ src/Ding/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello, my name is Isabel Ding.");
         System.out.println("Hello Isabel! Its Reece.");
+        System.out.println("Hello Isabel. My name is Robert.");
     }
 }
