6fd74517c8e60cd94dc08fa100f3d2802da3fe88
Reece <bro18061@byui.edu>
2020-01-31 16:38:28 -0700

    Hey Isabel

--- src/Ding/Main.java
+++ src/Ding/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Isabel Ding.");
+        System.out.println("Hello Isabel! Its Reece.");
     }
 }
